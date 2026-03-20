6a66ec2bafd9c607affb24de3f3347e3c5bfb100
Tom Boone <tom.boone@ingest.ai>
2026-03-20 15:54:45 -0400

    config cleanup

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-tbc-app-services"
+    storage_account_name = "tbcterraformstate"
+    container_name       = "tfstate"
+    key                  = "tomboone-dot-com.tfstate"
+    use_oidc             = true
+  }
 }
